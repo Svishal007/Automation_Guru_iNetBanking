2fdb2a3afa5ac8e6ebdad47f6f4f34b767e367a4
SVishal007 <sonkusare.vishal1@gmail.com>
2020-04-09 22:41:11 +0530

    iNetBanking Repractive-Added Login Page Objects

--- src/test/java/com/iNetBankingGuru/pageObjects/LoginPageDemoGuru.java
+++ src/test/java/com/iNetBankingGuru/pageObjects/LoginPageDemoGuru.java
@@ -1,6 +1,48 @@
 package com.iNetBankingGuru.pageObjects;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPageDemoGuru {
+	
+	WebDriver ldriver;
+	
+	public LoginPageDemoGuru(WebDriver rdriver ) {
+		ldriver=rdriver;
+		PageFactory.initElements(rdriver, this);
+		
+	}
+
+	@FindBy(name="uid")
+	WebElement txtUsername;
+	
+	@FindBy(name="password")
+	WebElement txtPassword;
+	
+	@FindBy(name="btnLogin")
+	WebElement btnSubmit;
+	
+	@FindBy(name="btnReset")
+	WebElement btnReset;
+
+	
+	public void setUsername(String uname) {
+		txtUsername.sendKeys(uname);
+	}
+	public void setPassword(String pwd) {
+		txtPassword.sendKeys(pwd);
+	}
+	public void clickSubmit() {
+		btnSubmit.click();
+	}
+	public void clickReset() {
+		btnReset.click();
+	}
+	
+	
 	
 	
 
